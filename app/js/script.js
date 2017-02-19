c74dd6edfcee717bb28dbe4ffcccdb4b425e8186
Your Name <you@example.com>
2017-02-19 18:18:52 +0200

    add adaptive for mobile

--- app/js/script.js
+++ app/js/script.js
@@ -1,23 +1,25 @@
 var navbar = document.getElementById('navbar'); 
-var swiper = new Swiper('.swiper-container', {
-        pagination: '.swiper-pagination',
-        slidesPerView: 2,
-        centeredSlides: true,
-        paginationClickable: true,
-        spaceBetween: 30,
-        grabCursor: true,
-        loop: true,
-        parallax: true,
-        nextButton: '.swiper-button-next',
-   		prevButton: '.swiper-button-prev'
-    });
 
- window.onscroll = function(){
-    if(document.body.scrollTop > 540) {
-        navbar.style.backgroundColor = 'black';
-    }else {
-        navbar.style.backgroundColor = 'transparent';
+$('#navbarBtn').click(function(){
+    $('#navMenu').slideToggle(300);
+})
+
+var swiper = new Swiper('.swiper-container', {
+    pagination: '.swiper-pagination',
+    slidesPerView: 2,
+    centeredSlides: true,
+    paginationClickable: true,
+    spaceBetween: 30,
+    grabCursor: true,
+    loop: true,
+    parallax: true,
+    nextButton: '.swiper-button-next',
+    prevButton: '.swiper-button-prev'
+});
+
+    if ($(window).width() <= 768){  
+         swiper.slidesPerView = 1;
     }
 
- }
+
 
